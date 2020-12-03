ee0a1682e691b2245f2ceea244bdea4f32e1588c
Wellington Carlos Massola <wellingtonmassola@gmail.com>
2020-12-03 18:22:19 -0300

    BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
+import React from 'react';
+import { HashRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import GlobalStyle from './styles/global';
-import Routes from './routes'
-import Navbar from "./Components/Navbar";
+import Routes from './routes';
+import Navbar from './Components/Navbar';
 
 const App: React.FC = () => (
   <Router>
